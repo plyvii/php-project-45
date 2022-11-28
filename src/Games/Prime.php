92a16498688eda66c1085e126513f5cf4c880b67
Irina <plyvchirina@gmail.com>
2022-11-28 14:31:01 +0300

    step 9

---
+++ src/Games/Prime.php
@@ -0,0 +1,10 @@
+<?php
+namespace Brain\Games\Prime;
+
+use function Brain\Games\Engine\someGame;
+
+function isPrime($playerName, $question, $correctAnswer)
+{
+$condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+someGame($playerName, $condition, $question, $correctAnswer);
+}
